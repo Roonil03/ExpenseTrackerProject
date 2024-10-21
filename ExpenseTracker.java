e440a3dd9a712004c69b34e8eb6d6a62718700ac
SUPERSUPERSUPERuser <102727268+SUPERSUPERSUPERuser@users.noreply.github.com>
2024-10-21 18:48:23 +0530

    Add files via upload

---
+++ ExpenseTracker.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpenseTracker {
+    private List<Expense> expenses;
+
+    public ExpenseTracker() {
+        expenses = new ArrayList<>();
+    }
+
+    public void addExpense(Expense expense) {
+        expenses.add(expense);
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public double getTotalExpenses() {
+        double total = 0;
+        for (Expense expense : expenses) {
+            total += expense.getAmount();
+        }
+        return total;
+    }
+}
+
